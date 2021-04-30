0de8ec6b516c378c20919703565ce293bede2224
Konserwa203 <MaciejKolber98@gmail.com>
2021-04-30 16:00:16 +0200

    Enabled fluent UserValidator for User class

--- RezerwacjaPOL/RezerwacjaPOL/Startup.cs
+++ RezerwacjaPOL/RezerwacjaPOL/Startup.cs
@@ -26,9 +26,11 @@ namespace RezerwacjaPOL
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-             
+            services.AddControllersWithViews()
+                .AddFluentValidation();
+            services.AddTransient<IValidator<User>, UserValidator>();
             
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
