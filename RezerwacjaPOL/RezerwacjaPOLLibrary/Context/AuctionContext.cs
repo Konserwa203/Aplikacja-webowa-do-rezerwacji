acdff265c8254ebf22a4ba898c6697af664e6e40
Konserwa203 <MaciejKolber98@gmail.com>
2021-05-06 21:05:53 +0200

    Installed packages for EntityFramework.Core/SqlServer, created DbContext - AuctionContext, and updated fluentvalidation package due to problems with compiling

---
+++ RezerwacjaPOL/RezerwacjaPOLLibrary/Context/AuctionContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RezerwacjaPOLLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezerwacjaPOLLibrary.Context
+{
+    public class AuctionContext : DbContext
+    {
+        public DbSet<Auction> Auctions { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+             // Todo - podmienić
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
