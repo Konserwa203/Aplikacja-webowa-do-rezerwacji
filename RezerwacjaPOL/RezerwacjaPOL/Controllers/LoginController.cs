9afc9ac2773066753209c84d3f594d21e8eef91e
Fifok <fifok77@gmail.com>
2021-05-09 17:24:46 +0200

    Add IndexPost action to LoginController

--- RezerwacjaPOL/RezerwacjaPOL/Controllers/LoginController.cs
+++ RezerwacjaPOL/RezerwacjaPOL/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RezerwacjaPOL.Models;
 using RezerwacjaPOLLibrary.Context;
+using RezerwacjaPOLLibrary.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,21 @@ namespace RezerwacjaPOL.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [ActionName("Index")]
+        public IActionResult IndexPost(LoginViewModel loginData)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _context.Users.FirstOrDefault(x => x.Email == loginData.Email && x.Password == loginData.Password);
+                if(user != null)
+                {
+                    return Ok(user);
+                }
+            }
+
+            return NotFound();
         }
         
     }
