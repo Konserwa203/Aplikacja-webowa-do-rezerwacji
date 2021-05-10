fdfa6bbb61266fc0363c8ef7d1563b8b59f0674c
Konserwa203 <MaciejKolber98@gmail.com>
2021-05-10 13:23:04 +0200

    added class for user vm tests

---
+++ RezerwacjaPOL/RezerwacjePOLTest/UserVMValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using RezerwacjaPOL.Models.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RezerwacjaPOLLibrary.Validators;
+
+namespace RezerwacjePOLTest
+{
+    [TestFixture]
+    public class UserVMValidatorTests
+    {
+       
+
+    }
+}
