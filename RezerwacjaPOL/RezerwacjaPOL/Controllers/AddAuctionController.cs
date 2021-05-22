0ea244030379289947cb24b2b40bd303418da936
Mateusz Bujak <62397807+M-Bujak@users.noreply.github.com>
2021-05-23 00:05:47 +0200

    added static class ImageTool used for saving files and other image manipulating features

--- RezerwacjaPOL/RezerwacjaPOL/Controllers/AddAuctionController.cs
+++ RezerwacjaPOL/RezerwacjaPOL/Controllers/AddAuctionController.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RezerwacjaPOL.Models;
+using RezerwacjaPOLLibrary.Context;
+using RezerwacjaPOLLibrary.Models;
+using RezerwacjaPOLLibrary.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +15,25 @@ namespace RezerwacjaPOL.Controllers
 {
     public class AddAuctionController : Controller
     {
+        private static IHostingEnvironment _enviroment;
+        private static IConfiguration _configuration;
+        private static AuctionContext _context;
+
+        public AddAuctionController(IHostingEnvironment environment, IConfiguration configuration, AuctionContext context)
+        {
+            _enviroment = environment;
+            _configuration = configuration;
+            _context = context;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AuctionViewModel newAuction)
+        {
+
             return View();
         }
     }
---
+++ RezerwacjaPOL/RezerwacjaPOL/ImageTool.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using RezerwacjaPOLLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RezerwacjaPOL
+{
+    public static class ImageTool
+    {
+        //This class is created as a demonstration of the facade design pattern.
+        static string SaveImage(IFormFile image, IHostingEnvironment _environment)
+        {
+            if (image.Length > 0)
+            {
+                string wwwrootPath = _environment.WebRootPath;
+                string fileExtension = Path.GetExtension(image.FileName);
+                var guid = Guid.NewGuid().ToString().Substring(0, 4);
+                string filePathLocal = guid + fileExtension;
+                var filePathRoot = Path.Combine(wwwrootPath + "/Files/" + filePathLocal);
+
+                using (var stream = System.IO.File.Create(filePathRoot))
+                {
+                    image.CopyTo(stream);
+                    return filePathLocal;
+                }
+            }
+            else return "default.png";
+        }
+    }
+}
