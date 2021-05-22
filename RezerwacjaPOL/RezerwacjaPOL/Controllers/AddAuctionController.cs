2d81cb9206cab73e062ea66a7aa7849198a988c6
Mateusz Bujak <62397807+M-Bujak@users.noreply.github.com>
2021-05-22 20:11:18 +0200

    Added AddAuction View

--- RezerwacjaPOL/RezerwacjaPOL/Controllers/AddAuctionController.cs
+++ RezerwacjaPOL/RezerwacjaPOL/Controllers/AddAuctionController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RezerwacjaPOL.Controllers
 {
-    public class AddActionController : Controller
+    public class AddAuctionController : Controller
     {
         public IActionResult Index()
         {
