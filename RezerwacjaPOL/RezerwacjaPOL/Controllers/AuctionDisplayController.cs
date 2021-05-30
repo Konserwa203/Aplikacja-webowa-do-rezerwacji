f965862afbd2de25e4ddc18cfa083ad5848c2a16
Konserwa203 <MaciejKolber98@gmail.com>
2021-05-30 13:38:40 +0200

    Controller now passess clicked auction to view

--- RezerwacjaPOL/RezerwacjaPOL/Controllers/AuctionDisplayController.cs
+++ RezerwacjaPOL/RezerwacjaPOL/Controllers/AuctionDisplayController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RezerwacjaPOL.Models;
+using RezerwacjaPOLLibrary.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,30 @@ namespace RezerwacjaPOL.Controllers
 {
     public class AuctionDisplayController : Controller
     {
+        private readonly AuctionContext _context;
+        
+
+        public AuctionDisplayController(AuctionContext context)
+        {
+            _context = context;
+        
+        }
+
         public IActionResult Index(int Id =0)
         {
-            return View();
+
+            var getSingleAuction = _context.Auctions.Include(x => x.PhotosPath).ThenInclude(x => x.Auction.Category).Where(x=>x.Id==Id).ToList().First();
+            var auction = new AuctionViewModel
+            {
+                Title = getSingleAuction.Title,
+                ThumbnailPhotoDir = getSingleAuction.PhotosPath.Select(x => x.PhotoPath).FirstOrDefault(),
+                Category = getSingleAuction.Category,
+                DateAdded = getSingleAuction.CreatedOn,
+                PhotosPath = getSingleAuction.PhotosPath,
+                Description = getSingleAuction.Description,
+                Id = getSingleAuction.Id
+            };
+            return View(auction);
         }
     }
 }
