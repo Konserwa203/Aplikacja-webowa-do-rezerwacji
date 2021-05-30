b96ea33c07dc3b4dd8b5fee890976102dd284c09
Konserwa203 <MaciejKolber98@gmail.com>
2021-05-30 13:25:22 +0200

    Created AuctionDisplay Controller and View

---
+++ RezerwacjaPOL/RezerwacjaPOL/Controllers/AuctionDisplayController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RezerwacjaPOL.Controllers
+{
+    public class AuctionDisplayController : Controller
+    {
+        public IActionResult Index(int Id =0)
+        {
+            return View();
+        }
+    }
+}
