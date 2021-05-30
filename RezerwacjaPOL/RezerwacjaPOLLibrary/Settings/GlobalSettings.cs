2b0d5cc811aac15251fa4f2a03838a7ad1370872
Konserwa203 <MaciejKolber98@gmail.com>
2021-05-30 14:09:49 +0200

    Used DI for httpcontext

---
+++ RezerwacjaPOL/RezerwacjaPOLLibrary/Settings/GlobalSettings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezerwacjaPOLLibrary.Settings
+{
+    public class GlobalSettings : IGlobalSettings
+    {
+        private IHttpContextAccessor _accessor;
+
+        public GlobalSettings(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+       
+
+       
+    }
+}
