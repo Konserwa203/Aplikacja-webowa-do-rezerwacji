04c7b5d55136bf6edec1b6b0d970a0c3816475c6
TomaszBakATH <technologiatomasz@gmail.com>
2021-05-31 18:23:38 +0200

    Adding 3 asserts for SearchEngineValidate

--- RezerwacjaPOL/RezerwacjaPOL/Models/Validators/SearchVMValidator.cs
+++ RezerwacjaPOL/RezerwacjaPOL/Models/Validators/SearchVMValidator.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using FluentValidation;
+using RezerwacjaPOL.Models;
+using RezerwacjaPOLLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RezerwacjaPOL.Models.Validators
 {
-    public class SearchVMValidator
+    public class SearchVMValidator : AbstractValidator<SearchEngineModel>
     {
+        public SearchVMValidator()
+        {
+             RuleFor(x=>x.Title).NotEmpty().Length(1, 1000);
+             RuleFor(x => x.Description).NotEmpty().Length(5, 1000);
+             RuleFor(x => x.PhoneNumber).NotEmpty().Length(6, 12);
+        }
+       
     }
 }
--- RezerwacjaPOL/RezerwacjePOLTest/SearchVMValidatorTests.cs
+++ RezerwacjaPOL/RezerwacjePOLTest/SearchVMValidatorTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using RezerwacjaPOL.Models;
+using RezerwacjaPOL.Models.Validators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,48 @@ using System.Threading.Tasks;
 
 namespace RezerwacjePOLTest
 {
-    class SearchVMValidatorTests
+    [TestFixture]
+    public class SearchVMValidatorTests
     {
+        [Test]
+        public void ShouldNotValidatePhoneNumberIsTooLong()
+        {
+            var searchEM = new SearchEngineModel
+            {
+                PhoneNumber = "11111111111111111111111111111111111",
+                Title="Tytuł 1234567",
+                Description="Opis 123456789"               
+            };
+            var validator = new SearchVMValidator();
+            var result = validator.Validate(searchEM);
+            Assert.IsFalse(result.IsValid);
+        }
+        [Test]
+        public void ShouldValidateTitleAndDesriptionHaveCorrectLenght()
+        {
+            var searchEM = new SearchEngineModel
+            {
+                PhoneNumber = "111111111",
+                Title = "Tytuł 1234567",
+                Description = "Opis 123456789"
+            };
+            var validator = new SearchVMValidator();
+            var result = validator.Validate(searchEM);
+            Assert.IsTrue(result.IsValid);
+        }
+        [Test]
+        public void ShouldNotValidateDateAddedIsInFuture()
+        {
+            var searchEM = new SearchEngineModel
+            {
+                PhoneNumber = "111111111",
+                Title = "Tytuł 1234567",
+                Description = "Opis 123456789",
+                DateAdded = DateTime.Now.AddYears(20)                
+            };
+            var validator = new SearchVMValidator();
+            var result = validator.Validate(searchEM);
+            Assert.IsFalse(result.IsValid);
+        }
     }
 }
