c553d12bea5a55a5faaf63859d445a487e04f894
TomaszBakATH <technologiatomasz@gmail.com>
2021-05-31 18:04:56 +0200

    Adding tests

---
+++ RezerwacjaPOL/RezerwacjaPOL/Models/Validators/SearchVMValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RezerwacjaPOL.Models.Validators
+{
+    public class SearchVMValidator
+    {
+    }
+}
