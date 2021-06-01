a7ec7ff1feb218eaf4156878d7d8c5ac1fa43492
Maciej Kolber <61764341+Konserwa203@users.noreply.github.com>
2021-06-01 20:55:20 +0200

    Revert "Revert "Dev" - SEARCH engine causes add auction to throw exception"

---
+++ RezerwacjaPOL/RezerwacjePOLTest/SearchVMValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using RezerwacjaPOL.Models;
+using RezerwacjaPOL.Models.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezerwacjePOLTest
+{
+    [TestFixture]
+    public class SearchVMValidatorTests
+    {
+        [Test]
+        public void ShouldNotValidatePhoneNumberIsTooLong()
+        {
+            var searchEM = new SearchEngineModel
+            {
+                PhoneNumber = "11111111111111111111111111111111111",
+                Title="Tytuł 1234567",
+                Description="Opis 123456789"               
+            };
+            var validator = new SearchVMValidator();
+            var result = validator.Validate(searchEM);
+            Assert.IsFalse(result.IsValid);
+        }
+        [Test]
+        public void ShouldValidateTitleAndDesriptionHaveCorrectLenght()
+        {
+            var searchEM = new SearchEngineModel
+            {
+                PhoneNumber = "111111111",
+                Title = "Tytuł 1234567",
+                Description = "Opis 123456789"
+            };
+            var validator = new SearchVMValidator();
+            var result = validator.Validate(searchEM);
+            Assert.IsTrue(result.IsValid);
+        }
+        [Test]
+        public void ShouldNotValidateDateAddedIsInFuture()
+        {
+            var searchEM = new SearchEngineModel
+            {
+                PhoneNumber = "111111111",
+                Title = "Tytuł 1234567",
+                Description = "Opis 123456789",
+                DateAdded = DateTime.Now.AddYears(20)                
+            };
+            var validator = new SearchVMValidator();
+            var result = validator.Validate(searchEM);
+            Assert.IsFalse(result.IsValid);
+        }
+    }
+}
