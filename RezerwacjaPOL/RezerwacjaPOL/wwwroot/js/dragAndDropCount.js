08fb6d0cf73e49e913ded10c0712e8b099077531
Konserwa203 <MaciejKolber98@gmail.com>
2021-06-01 19:24:13 +0200

    added javascript which shows uploaded filenames

---
+++ RezerwacjaPOL/RezerwacjaPOL/wwwroot/js/dragAndDropCount.js
@@ -0,0 +1,17 @@
+﻿
+    let inputFile = document.getElementById('drop-zone');
+    let fileNmeField = document.getElementById('howMany');
+    inputFile.addEventListener('change', function (event) {
+        let uploadedFileNames = event.target.files.length;
+        let names = event.target.files;
+        var files = "";
+        var textToRead = "";
+        for (var i = 0; i < uploadedFileNames; i++) {
+            files = files.concat(names[i].name, ", ");
+          
+        }
+        
+        fileNmeField.textContent = "Pliki do przesłania: " + files;
+    })
+
+
