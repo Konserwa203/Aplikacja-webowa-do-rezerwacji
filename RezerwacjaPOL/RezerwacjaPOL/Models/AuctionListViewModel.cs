82643111cc95f13ceaf3ae9fbbaef7e3a0fe1331
Fifok <fifok77@gmail.com>
2021-06-07 21:14:48 +0200

    Add AuctionListViewModel

---
+++ RezerwacjaPOL/RezerwacjaPOL/Models/AuctionListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RezerwacjaPOL.Models
+{
+    public class AuctionListViewModel
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public IEnumerable<AuctionViewModel> Auctions { get; set; }
+    }
+}
