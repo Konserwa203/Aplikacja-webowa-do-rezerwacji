411e39a36d5cf834f01b476c73d28c3c82f57efe
Mateusz Bujak <62397807+M-Bujak@users.noreply.github.com>
2021-06-14 16:39:22 +0200

    Added tests for Auction View Model

--- RezerwacjaPOL/RezerwacjePOLTest/AddAuctionVMValidatorTests.cs
+++ RezerwacjaPOL/RezerwacjePOLTest/AddAuctionVMValidatorTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using RezerwacjaPOL.Models;
+using RezerwacjaPOLLibrary.Validators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,46 @@ namespace RezerwacjePOLTest
 {
     class AddAuctionVMValidatorTests
     {
+        [Test]
+        public void ShouldNotValidateIfTitleIsTooShort()
+        {
+            var auctionVM = new AuctionViewModel
+            {
+                Title = "short",
+                Description = "Description is of good length",
+                PhoneNumber = "12345678910"
+            };
+            var validator = new AuctionVMValidator();
+            var result = validator.Validate(auctionVM);
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [Test]
+        public void ShouldValidateIfEveryPropertyIsOfGoodLength()
+        {
+            var auctionVM = new AuctionViewModel
+            {
+                Title = "Title is of good length",
+                Description = "Description is of good length",
+                PhoneNumber = "12345678910"
+            };
+
+            var validator = new AuctionVMValidator();
+            var result = validator.Validate(auctionVM);
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        public void ShouldNotValidateIfPhoneNumberIsNotProvided()
+        {
+            var auctionVM = new AuctionViewModel
+            {
+                Title = "short",
+                Description = "Description is of good length",
+            };
+            var validator = new AuctionVMValidator();
+            var result = validator.Validate(auctionVM);
+            Assert.IsFalse(result.IsValid);
+        }
     }
 }
